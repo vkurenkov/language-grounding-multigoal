d41b8c5b96b2b6f01afdf75bd633b63ced277bab
Vladislav Kurenkov <vvkurenkov@mail.ru>
2019-01-18 21:18:27 +0300

    [utils] Add experiment folder creation

--- utils/training.py
+++ utils/training.py
@@ -1,6 +1,7 @@
 import torch
 import numpy
 import random
+import os
 
 
 def fix_random_seeds(seed: int) -> None:
@@ -13,3 +14,20 @@ def fix_random_seeds(seed: int) -> None:
     
     numpy.random.seed(seed)
     random.seed(seed)
+
+def create_experiment_folder(root_path: str, env_name: str, agent_name: str) -> str:
+    experiment_path = os.path.join(root_path, env_name, agent_name)
+
+    if os.path.isdir(experiment_path):
+        print("This experiment already exists. Do you want to proceed (erase the old one)? (y/n)")
+        answer = input()
+        if answer == "y":
+            os.rmdir(experiment_path)
+        else:
+            # Stop the experiment
+            exit(0)
+
+    # Make sure we do not overwrite existing experiments
+    os.makedirs(experiment_path, exist_ok=False)
+
+    return experiment_path
