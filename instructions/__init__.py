7af5188dce691347d30045e5c6d3a1cab744abaf
Vladislav Kurenkov <vvkurenkov@mail.ru>
2019-04-14 19:04:45 +0300

    [instructions] Change interface for extracting the instructions 	- Make one access point where you specify level and etc.

--- instructions/__init__.py
+++ instructions/__init__.py
@@ -12,23 +12,16 @@ from utils.training                                import fix_random_seeds
 def get_this_file_path() -> str:
     return os.path.dirname(__file__)
 
-def get_instructions(path: str) -> List[NaturalLanguageInstruction]:
+def read_instructions_from_disk(path: str) -> List[NaturalLanguageInstruction]:
     dataset_path = os.path.join(path)
     with open(dataset_path, mode="r") as f:
         dataset = json.load(f)
 
-    return [[instruction["raw"], instruction["objects_real_order"]] for instruction in dataset["instructions"]]
+    return [(instruction["raw"], instruction["objects_real_order"]) for instruction in dataset["instructions"]]
 
-def get_level0_instructions() -> List[NaturalLanguageInstruction]:
-    dataset_path = os.path.join(get_this_file_path(), "level0.json")
-    return get_instructions(dataset_path)
-
-def get_instructions_tokenizer(instructions: NaturalLanguageInstruction, padding_len=None) -> InstructionTokenizer:
-    return InstructionTokenizer([instr[0] for instr in instructions], padding_len)
-
-def get_level1_instructions(max_train_subgoals: int, unseen_proportion: float, seed: int) -> Tuple[List[NaturalLanguageInstruction], List[NaturalLanguageInstruction], List[NaturalLanguageInstruction]]:
+def get_instructions(level: int, max_train_subgoals: int, unseen_proportion: float, seed: int):
     """
-    Divides the Level1 dataset into 3 parts: training instructions, unseen combinations, and higher number of subgoals.
+    Divides the specified level dataset into 3 parts: training instructions, unseen combinations, and higher number of subgoals.
 
     :param max_train_subgoals: Maximum number of available sub-goals during training. Others will go to the 3rd part. 
     :param unseen_proportion: Split between training instructions and unseen combinations.
@@ -37,10 +30,10 @@ def get_level1_instructions(max_train_subgoals: int, unseen_proportion: float, s
     # We want our splits to be reproducible
     fix_random_seeds(seed)
 
-    dataset_path = os.path.join(get_this_file_path(), "level1.json")
-    instructions = get_instructions(dataset_path)
+    dataset_path = os.path.join(get_this_file_path(), "level{}.json".format(level))
+    instructions = read_instructions_from_disk(dataset_path)
 
-    print("Level1: Total {} instructions.".format(len(instructions)))
+    print("Level{}: Total {} instructions.".format(level, len(instructions)))
 
     higher_num_subgoals = []
     training            = []
@@ -51,16 +44,19 @@ def get_level1_instructions(max_train_subgoals: int, unseen_proportion: float, s
         else:
             training.append(instruction)
 
-    training = np.array(training)
+    training = np.array(training, dtype=object)
     np.random.shuffle(training)
 
     num_unseen = int(len(training) * unseen_proportion)
     unseen     = training[:num_unseen]
     training   = training[num_unseen:]
 
-    print("Level1: Training {} instructions.".format(len(training)))
-    print("Level1: Unseen {} instructions.".format(len(unseen)))
-    print("Level1: Higher subgoals {} instructions.".format(len(higher_num_subgoals)))
+    print("Level{}: Training {} instructions.".format(level, len(training)))
+    print("Level{}: Unseen {} instructions.".format(level, len(unseen)))
+    print("Level{}: Higher subgoals {} instructions.".format(level, len(higher_num_subgoals)))
 
     return training.tolist(), unseen.tolist(), higher_num_subgoals
+
+def get_instructions_tokenizer(instructions: NaturalLanguageInstruction, padding_len=None) -> InstructionTokenizer:
+    return InstructionTokenizer([instr[0] for instr in instructions], padding_len)
     
